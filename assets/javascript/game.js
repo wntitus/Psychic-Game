f97fbef38938f642f19319bbc5a1de9a69a2706e
wntitus <wesn.titus@gmail.com>
2018-07-18 12:08:40 -0400

    adding commentsd

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,32 @@
+// declaring variables for alphabet and pc guess
+
+
 var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var pcGuess = alpha[Math.floor(Math.random() * alpha.length)];
+
+// declaring variables for wins, losses, guesses, and empty array to store player guesses
 
 var wins = 0;
 var losses = 0;
 var guessLeft = 8;
 var guesses = [];
 
+// main logic, when player presses a letter triggers the game to begin
+
 document.onkeyup = function(event){
 
     console.log(event.key);
-    
+    console.log(typeof event.key);
+
     if (event.key == alpha) {
+
+        // storing user keypress in variable and adding pressed key into the guesses array
 
         var userGuess = event.key;
         guesses.push(event.key);
+
+        // altering html body to contain game info
 
         var newBody = 
             "<p>You chose: " + userGuess + "</p>" +
@@ -24,6 +36,8 @@ document.onkeyup = function(event){
         document.querySelector("#gameBody").innerHTML = newBody;
 
         console.log(pcGuess);
+
+        //logic conditions to alter wins or losses, also ends game and resets if player wins or loses
 
         if (userGuess == pcGuess) {
             wins++;
