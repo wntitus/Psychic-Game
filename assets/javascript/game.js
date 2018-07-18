cb9721b6a9fe37d6911ac0600200b67a836becff
wntitus <wesn.titus@gmail.com>
2018-07-18 11:53:21 -0400

    working on game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,17 +9,37 @@ var guesses = [];
 
 document.onkeyup = function(event){
 
-    var userGuess = event.key;
-    guesses.push(event.key);
-
-    var newBody = 
-        "<p>You chose: " + userGuess + "</p>" +
-        "<p>You have " + guessLeft + " guesses left.</p>" +
-        "<p>You've guessed " + guesses + " so far.</p>";
-
-    document.querySelector("#gameBody").innerHTML = newBody;
-
-    console.log(pcGuess);
-
+    console.log(event.key);
     
+    if (event.key == alpha) {
+
+        var userGuess = event.key;
+        guesses.push(event.key);
+
+        var newBody = 
+            "<p>You chose: " + userGuess + "</p>" +
+            "<p>You have " + guessLeft + " guesses left.</p>" +
+            "<p>You've guessed " + guesses + " so far.</p>";
+
+        document.querySelector("#gameBody").innerHTML = newBody;
+
+        console.log(pcGuess);
+
+        if (userGuess == pcGuess) {
+            wins++;
+            guessLeft == 8;
+            alert("You have won! Your guesses have been reset.");
+        }
+
+        if (userGuess != pcGuess) {
+            guessLeft--;
+        }
+
+        if (guessLeft == 0) {
+            losses++;
+            alert("You ran out of guesses and lost! The computer has picked a new letter.");
+            var pcGuess = alpha[Math.floor(Math.random() * alpha.length)];
+        }
+
+    }  
 }
