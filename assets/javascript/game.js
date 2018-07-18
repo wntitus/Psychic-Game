1df93e22152a4abd5a04bb6259b022ce2a5fa62d
wntitus <wesn.titus@gmail.com>
2018-07-18 11:40:21 -0400

    modified pc guess to be an actual letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var pcGuess = Math.floor(Math.random() * alpha.length);
+var pcGuess = alpha[Math.floor(Math.random() * alpha.length)];
 
 var wins = 0;
 var losses = 0;
@@ -10,15 +10,16 @@ var guesses = [];
 document.onkeyup = function(event){
 
     var userGuess = event.key;
-    var guesses = guesses.push(event.key);
+    guesses.push(event.key);
 
     var newBody = 
-    "<p>You chose: " + userGuess + "</p>" +
-    "<p>The computer chose: " + pcGuess + "</p>" +
-    "<p>You have " + guessLeft + " guesses left.</p>" +
-    "<p>You've guessed " + guesses + " so far.</p>"
+        "<p>You chose: " + userGuess + "</p>" +
+        "<p>You have " + guessLeft + " guesses left.</p>" +
+        "<p>You've guessed " + guesses + " so far.</p>";
 
     document.querySelector("#gameBody").innerHTML = newBody;
+
+    console.log(pcGuess);
 
     
 }
